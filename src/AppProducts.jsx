2f34897b155988518aec53b64ae6449c722267ce
danielmmk <daniel.montalvo@mmkgroup.com.mx>
2024-11-02 13:47:56 -0600

    Se agregan dependencias necesarias para el proyecto

---
+++ src/AppProducts.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material"
+
+
+function AppProductos() {
+
+  return (
+    <>
+    <Button variant="contained" color="success">
+      Success
+    </Button>
+    </>
+  )
+}
+
+export default AppProductos
